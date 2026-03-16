cddaff7057c70827f271f77d318d0b735fa2f066
Eliton <elitonfilho@gmail.com>
2026-03-16 22:15:04 +0100

    feat: accepting multiple geom types on keepType

--- geetools/ee_geometry.py
+++ geetools/ee_geometry.py
@@ -14,14 +14,14 @@ class GeometryAccessor:
         """Initialize the Geometry class."""
         self._obj = obj
 
-    def keepType(self, type: str) -> ee.Geometry:
-        """Only keep the geometries of the given type from a GeometryCollection.
+    def keepType(self, type: str | list[str]) -> ee.Geometry:
+        """Only keep the geometries of the given type(s) from a GeometryCollection.
 
         Args:
-            type: The type of geometries to keep. Can be one of: Point, LineString, LineRing Polygon.
+            type: The type of geometries to keep. Can be a single type or a list of types. Valid types are: Point, LineString, LinearRing, Polygon.
 
         Returns:
-            The geometries of the given type.
+            The geometries of the given type. If a single type is provided, a Multi{type} geometry is returned. If a list of types is provided, a GeometryCollection is returned.
 
         Examples:
             .. jupyter-execute::
@@ -50,14 +50,27 @@ class GeometryAccessor:
                 # extract only the LineString geometries from the collection
                 geom = geometryCollection.geetools.keepType('LineString')
                 geom.getInfo()
+
+            .. jupyter-execute::
+
+                # extract LineString and Point geometries from the collection
+                geom = geometryCollection.geetools.keepType(['LineString', 'Point'])
+                geom.getInfo()
         """
         # will raise an error if self is not a GeometryCollection
         error_msg = "This method can only be used with GeometryCollections"
         assert self._obj.type().getInfo() == "GeometryCollection", error_msg
 
+        types = type if isinstance(type, list) else [type]
+        type_list = ee.List(types)
+
         def filterType(geom):
             geom = ee.Geometry(geom)
-            return ee.Algorithms.If(geom.type().compareTo(type), None, geom)
+            return ee.Algorithms.If(type_list.containsAll(ee.List([geom.type()])), geom, None)
 
         geometries = self._obj.geometries().map(filterType, True)
-        return getattr(ee.Geometry, "Multi" + type)(geometries, self._obj.projection())
+
+        if len(types) == 1:
+            return getattr(ee.Geometry, "Multi" + types[0])(geometries, self._obj.projection())
+
+        return ee.Algorithms.GeometryConstructors.MultiGeometry(geometries, self._obj.projection())
--- tests/test_Geometry.py
+++ tests/test_Geometry.py
@@ -9,3 +9,39 @@ class TestKeepType:
         geojson = geom.getInfo()
         assert geojson["type"] == "MultiLineString"
         assert geom.coordinates().getInfo() == [[[0, 1], [0, 0]]]
+
+    def test_keep_type_point(self, geom_instance):
+        geom = geom_instance.geetools.keepType("Point")
+        geojson = geom.getInfo()
+        assert geojson["type"] == "MultiPoint"
+        assert geom.coordinates().getInfo() == [[0, 0]]
+
+    def test_keep_type_polygon(self, geom_instance):
+        geom = geom_instance.geetools.keepType("Polygon")
+        geojson = geom.getInfo()
+        assert geojson["type"] == "MultiPolygon"
+        assert len(geom.coordinates().getInfo()) == 2
+
+    def test_keep_type_list_single(self, geom_instance):
+        geom = geom_instance.geetools.keepType(["LineString"])
+        geojson = geom.getInfo()
+        assert geojson["type"] == "MultiLineString"
+        assert geom.coordinates().getInfo() == [[[0, 1], [0, 0]]]
+
+    def test_keep_type_list_multiple(self, geom_instance):
+        geom = geom_instance.geetools.keepType(["LineString", "Point"])
+        geojson = geom.getInfo()
+        assert geojson["type"] == "GeometryCollection"
+        types = [g["type"] for g in geojson["geometries"]]
+        assert "Point" in types
+        assert "LineString" in types
+        assert "Polygon" not in types
+
+    def test_keep_type_list_all(self, geom_instance):
+        geom = geom_instance.geetools.keepType(["LineString", "Point", "Polygon"])
+        geojson = geom.getInfo()
+        assert geojson["type"] == "GeometryCollection"
+        types = [g["type"] for g in geojson["geometries"]]
+        assert "Point" in types
+        assert "LineString" in types
+        assert "Polygon" in types
